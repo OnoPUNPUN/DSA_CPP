7202c48f1a6856b1f5f647c115ba0b0eb0ff939b
OnoPUNPUN <punpunonodera754@gmail.com>
2025-09-17 21:17:59 +0600

    .

--- Linked List/Double/Main.cpp
+++ Linked List/Double/Main.cpp
@@ -1,202 +1,149 @@
 #include <iostream>
 using namespace std;
 
-struct Node
+class Node
 {
+public:
     int data;
     Node *next;
     Node *prev;
+
+    Node()
+    {
+        this->data = 0;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+        this->prev = NULL;
+    }
 };
 
-void insertEnd(Node *&head, int data)
+class DoublyLinkedList
 {
-    Node *newNode = new Node;
-    newNode->data = data;
-    newNode->next = nullptr;
-    newNode->prev = nullptr;
+private:
+    Node *head;
 
-    if (head == nullptr)
+public:
+    DoublyLinkedList()
     {
+        head = NULL;
+    }
+
+    void insertAtBeginning(int value)
+    {
+
+        Node *newNode = new Node(value);
+        newNode->prev = NULL;
+        newNode->next = head;
+
+        if (head != NULL)
+            head->prev = newNode;
+
         head = newNode;
-        return;
     }
 
-    Node *cur = head;
-    while (cur->next != nullptr)
+    void insertAtEnd(int value)
     {
-        cur = cur->next;
+
+        Node *newNode = new Node(value);
+        newNode->next = NULL;
+
+        Node *temp = head;
+        while (temp->next != NULL)
+            temp = temp->next;
+
+        temp->next = newNode;
+        newNode->prev = temp;
     }
 
-    cur->next = newNode;
-    newNode->prev = cur;
-}
-
-void insertFirst(Node *&head, int data)
-{
-    Node *newNode = new Node;
-    newNode->data = data;
-    newNode->next = head;
-    newNode->prev = nullptr;
-    if (head != nullptr)
-        head->prev = newNode;
-    head = newNode;
-}
-
-void insertAfter(Node *head, int targetValue, int newValue)
-{
-    Node *cur = head;
-    while (cur != nullptr && cur->data != targetValue)
-    {
-        cur = cur->next;
-    }
-
-    if (cur == nullptr)
-    {
-        cout << "Value " << targetValue << " not found!\n";
-        return;
-    }
-
-    Node *newNode = new Node;
-    newNode->data = newValue;
-    newNode->next = cur->next;
-    newNode->prev = cur;
-
-    if (cur->next != nullptr)
-        cur->next->prev = newNode;
-    cur->next = newNode;
-}
-
-void deleteFirst(Node *&head)
-{
-    if (head == nullptr)
-    {
-        cout << "List empty\n";
-        return;
-    }
-
-    Node *temp = head;
-    head = head->next;
-    if (head != nullptr)
-        head->prev = nullptr;
-    delete temp;
-}
-
-void deleteLast(Node *&head)
-{
-    if (head == nullptr)
-    {
-        cout << "List empty\n";
-        return;
-    }
-
-    if (head->next == nullptr)
-    {
-        delete head;
-        head = nullptr;
-        return;
-    }
-
-    Node *cur = head;
-    while (cur->next != nullptr)
-    {
-        cur = cur->next;
-    }
-
-    if (cur->prev != nullptr)
-        cur->prev->next = nullptr;
-    delete cur;
-}
-
-void deleteAfter(Node *&head, int targetValue)
-{
-    if (head == nullptr)
-    {
-        cout << "List empty\n";
-        return;
-    }
-
-    Node *cur = head;
-    while (cur != nullptr && cur->data != targetValue)
-    {
-        cur = cur->next;
-    }
-
-    if (cur == nullptr)
-    {
-        cout << "Value " << targetValue << " not found!\n";
-        return;
-    }
-
-    if (cur->next == nullptr)
-    {
-        cout << "No node exists after " << targetValue << "\n";
-        return;
-    }
-
-    Node *target = cur->next;
-    cur->next = target->next;
-    if (target->next != nullptr)
-        target->next->prev = cur;
-    delete target;
-    cout << "Deleted node after " << targetValue << "\n";
-}
-
-void printList(Node *head)
-{
-    if (head == nullptr)
-    {
-        cout << "List empty\n";
-        return;
-    }
-
-    Node *cur = head;
-    while (cur != nullptr)
-    {
-        cout << cur->data << " ";
-        cur = cur->next;
-    }
-    cout << "\n";
-}
-
-void destroyList(Node *&head)
-{
-    while (head != nullptr)
+    void insertAfter(int key, int value)
     {
         Node *temp = head;
+        while (temp != NULL && temp->data != key)
+            temp = temp->next;
+
+        if (temp == NULL)
+        {
+            cout << "Node with value " << key << " not found.\n";
+            return;
+        }
+
+        Node *newNode = new Node(value);
+
+        newNode->next = temp->next;
+        newNode->prev = temp;
+
+        if (temp->next != NULL)
+            temp->next->prev = newNode;
+
+        temp->next = newNode;
+    }
+
+    void display()
+    {
+        Node *temp = head;
+        cout << "Doubly Linked List: ";
+        while (temp != NULL)
+        {
+            cout << temp->data << "->";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+
+    void deleteFromStart()
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty.\n";
+            return;
+        }
+
+        Node *temp = head;
         head = head->next;
+
+        if (head != NULL)
+            head->prev = NULL;
+
         delete temp;
     }
-}
+
+    void deleteFromEnd()
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty.\n";
+            return;
+        }
+
+        Node *temp = head;
+
+        while (temp->next != NULL)
+            temp = temp->next;
+
+        if (temp->prev != NULL)
+            temp->prev->next = NULL;
+        else
+            head = NULL;
+
+        delete temp;
+    }
+};
 
 int main()
 {
-    Node *head = nullptr;
-    insertFirst(head, 10);
-    insertEnd(head, 20);
-    insertEnd(head, 30);
-    insertEnd(head, 40);
 
-    cout << "Initial list: ";
-    printList(head);
+    DoublyLinkedList D;
+    D.insertAtBeginning(4);
+    D.insertAtEnd(5);
+    D.insertAtEnd(7);
 
-    insertFirst(head, 60);
-    insertAfter(head, 20, 80);
+    D.deleteFromStart();
+    D.display();
 
-    cout << "2nd list: ";
-    printList(head);
-
-    deleteFirst(head);
-    cout << "After deleting first: ";
-    printList(head);
-
-    deleteLast(head);
-    cout << "After deleting last: ";
-    printList(head);
-
-    deleteAfter(head, 80);
-    cout << "After deleting the node after 80: ";
-    printList(head);
-
-    destroyList(head);
     return 0;
 }
